b8180b5bee75d4fd4c3b32131a0a822847f01c50
johnk555 <170715815+johnk555@users.noreply.github.com>
2024-10-13 15:50:00 +0300

    Update person.c
    add some functions

--- Lesson13/person.c
+++ Lesson13/person.c
@@ -1,33 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #define SIZE 80
 
 struct person{
-    char onoma[SIZE];
-    char diefthinsi[SIZE];
-    char arithmos[SIZE];
-    char nomos[SIZE];
+    char *onoma;
+    char *diefthinsi;
+    char *arithmos;
+    char *nomos;
 };
 
 typedef struct person RECORD;
 
 void read_record(RECORD *p);
-
 void print_record(RECORD x);
+void init_record(RECORD *p);
+void free_record(RECORD x);
+void copy_record(RECORD *A,RECORD *x);
 
 int main()
 {
-    printf("Gia to atomo 1: ");
-    RECORD ena;
-    read_record(&ena);
+    RECORD x;
+    RECORD *pinakas;
+    int i,N;
+    int choice;
     
-    printf("Gia to atomo 2: ");
-    RECORD dyo;
-    read_record(&dyo);
+    printf("Dwse ton arithmo ton eggrafon: ");
+    scanf("%d",&N);
     
-    print_record(ena);
-    print_record(dyo);
+    pinakas=malloc(sizeof(RECORD)*N);
+    
+    if(!pinakas)
+    {
+        printf("Adynamia desmeusis mnimis!");
+        exit(0);
+    }
+    
+    for(i=0; i<N; i++)
+        init_record(&pinakas[i]);
+    init_record(&x);
+    
+    for(i=0; i<N; i++)
+    {
+        printf("Gia to atomo %d: ",i+1);
+        read_record(&pinakas[i]);
+    }
+    
+    printf("Apo poio atomo na antigrafoun ta stoixeia tou");
+    scanf("%d",&choice);
+    
+    copy_record(&pinakas[choice-1],&x);
+    
+    print_record(x);
+    
+    for(i=0; i<N; i++)
+        print_record(pinakas[i]);
+    
+    for(i=0; i<N; i++)
+        free_record(pinakas[i]);
+        
+
+    free_record(x);
+    free(pinakas);
     
 	return 0;
 }
@@ -49,9 +82,60 @@ void read_record(RECORD *p)
 
 void print_record(RECORD x)
 {
-    printf("To onoma tou atomou einai: %s\n", x.onoma );
-    printf("H diefthinsi tou atomou einai: %s\n", x.diefthinsi );
-    printf("O arithmos tou atomou einai: %s\n", x.arithmos );
-    printf("O nomos tou atomou einai: %s\n", x.nomos );
+    printf("%s : %s %s %s\n", x.onoma, x.diefthinsi, x.arithmos, x.nomos);
     
 }
+
+void init_record(RECORD *p)
+{
+    p->onoma = malloc(sizeof(char)*SIZE);
+    if(!p->onoma)
+    {
+        printf("Adynamia desmeusis mnimis");
+        exit(0);
+    }
+    
+    p->diefthinsi = malloc(sizeof(char)*SIZE);
+    if(!p->diefthinsi)
+    {
+        printf("Adynamia desmeusis mnimis");
+        exit(0);
+    }
+    
+    p->arithmos = malloc(sizeof(char)*SIZE);
+    if(!p->arithmos)
+    {
+        printf("Adynamia desmeusis mnimis");
+        exit(0);
+    }
+    
+    p->nomos = malloc(sizeof(char)*SIZE);
+    if(!p->nomos)
+    {
+        printf("Adynamia desmeusis mnimis");
+        exit(0);
+    }
+    
+}
+
+void free_record(RECORD x)
+{
+    free(x.onoma);
+    free(x.diefthinsi);
+    free(x.arithmos);
+    free(x.nomos);
+}
+
+void copy_record(RECORD *A,RECORD *x)
+{
+    x->onoma=A->onoma;
+    x->diefthinsi=A->diefthinsi;
+    x->arithmos=A->arithmos;
+    x->nomos=A->nomos;
+    
+}
+
+
+
+
+
