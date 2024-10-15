24cd4471f7b94dfa019203912044363dc41dc8c6
johnk555 <karelasgiannis@gmail.com>
2024-10-15 20:17:14 +0300

    Create redirect_output.c

---
+++ Lesson17/redirect_output.c
@@ -0,0 +1,15 @@
+/* Tha to xrisimopoiisoume gia anakateuthinsi eksodou */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+main(){
+
+    int i;
+    srand(time(NULL));
+
+    for(i=0;i<20;i++){
+        printf("%d\n",rand()%1000);
+  }
+}
