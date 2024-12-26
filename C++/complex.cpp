4ae2703ad8d9ead5ad0527de4cb0f2591b33c8ac
johnk555 <karelasgiannis@gmail.com>
2024-12-26 16:26:43 +0000

    Update complex.cpp

--- C++/complex.cpp
+++ C++/complex.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
+
 class complex {
-    public:
+public:
     complex();
     complex(double in_real, double in_imag);
     void set_real(double in_real);
@@ -14,93 +15,110 @@ class complex {
     complex operator- (const complex &right);
     complex operator++(int right);
     complex &operator++();
-    complex &operator=(const complex &right);
-    complex &operator=(int right);
-
-    private:
+    friend complex operator+ (int left, complex &right);
+    friend ostream &operator<<(ostream &left, const complex &right);
+private:
     double real;
     double imag;
-
 };
 
-int main() {
+complex operator+ (int left, complex &right);
+
+int main()
+{
     complex a(1.0,1.0);
-    //complex b(2.0,3.0);
 
-    complex c;
-
-    c=a;
-
-    cout<<"a="<<a.get_real()<<","<<a.get_imag()<<endl;
-    cout<<"c="<<c.get_real()<<","<<c.get_imag()<<endl;
-
+    cout << a << endl;
     return 0;
 }
-complex::complex() {
+
+complex::complex()
+{
     real = 0.0;
     imag = 0.0;
 }
-complex::complex(double in_real, double in_imag) {
+
+complex::complex(double in_real, double in_imag)
+{
     real = in_real;
     imag = in_imag;
 }
-void complex::set_real(double in_real) {
-    real=in_real;
+
+void complex::set_real(double in_real)
+{
+    real = in_real;
 }
-void complex::set_imag(double in_imag) {
-    imag=in_imag;
+
+void complex::set_imag(double in_imag)
+{
+    imag = in_imag;
 }
-double complex::get_real() const {
+
+double complex::get_real() const
+{
     return real;
 }
-double complex::get_imag() const {
+
+double complex::get_imag() const
+{
     return imag;
 }
 
-complex complex::operator+ (const complex &right) {
+complex complex::operator+ (const complex &right)
+{
     complex result;
 
-    result.real = real + right.real;
-    result.imag = imag + right.imag;
+    result.real=real+right.real;
+    result.imag=imag+right.imag;
 
     return result;
 }
-complex complex::operator+(int right) {
-    complex result;
-    result.real = real + right;
-    result.imag = imag + right;
-    return result;
-}
-complex complex::operator- (const complex &right) {
+
+complex complex::operator+ (int right)
+{
     complex result;
 
-    result.real = real - right.real;
-    result.imag = imag - right.imag;
+    result.real=real+5;
+    result.imag=imag+5;
 
     return result;
 }
-complex complex::operator++(int right) {
-    complex result= *this;
+
+complex complex::operator- (const complex &right)
+{
+    complex result;
+
+    result.real=real-right.real;
+    result.imag=imag-right.imag;
+
+    return result;
+}
+
+complex complex::operator++(int right)
+{
+    complex result=*this;
+
     real++;
     imag++;
+
     return result;
 }
-complex &complex::operator++() {
+
+complex &complex::operator++()
+{
     real++;
     imag++;
-    return *this;
-}
-complex &complex::operator=(const complex &right) {
-    if (this == &right) return *this;
-    real = right.real;
-    imag = right.imag;
 
     return *this;
 }
-
-complex &complex::operator=(int right) {
-    real = right;
-    imag = right;
-    return *this;
+complex operator+ (int left, complex &right) {
+    complex result;
+    result.real=left+right.real;
+    result.imag=left+right.imag;
+    return result;
 }
+ostream &operator<<(ostream &left, const complex &right) {
+    left<<"(" << right.real<<"," << right.imag<<")";
 
+    return left;
+}
