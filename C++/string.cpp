04a65a939b70a73403fe529afe4d9befcfc62f2a
johnk555 <karelasgiannis@gmail.com>
2024-12-26 16:40:41 +0000

    Update string.cpp

--- C++/string.cpp
+++ C++/string.cpp
@@ -1,4 +1,4 @@
-/* cpp6.askisi1.1.cpp */
+/* cpp6.askisi1.3.cpp */
 
 #include <iostream>
 #include <cstring>
@@ -10,19 +10,19 @@ class STRING {
         STRING(char *in_str);
         STRING(const STRING &ob);
         ~STRING();
-        int get_length() const; 
-        char *get_str() const; 
+        int get_length() const;
+        char *get_str() const;
         void set_str(char *in_str);
-        void print(); 
+        void print();
         STRING &operator=(STRING &right);
         char &operator[](int i);
         bool operator<(STRING &right);
         bool operator>(STRING &right);
         bool operator==(STRING &right);
-
+        friend ostream &operator<<(ostream &left, STRING &right);
     private:
        int length;
-       char *str; 
+       char *str;
 };
 
 
@@ -31,23 +31,16 @@ int main()
     char s[80]="This is a string";
     STRING s1(s);
     STRING s2;
+
     s2=s1;
-    
-    s1[2]='a';
-    s1[3]='t';
-    
-    s1.print();
-    
-    cout<<endl;
-    
-    if(s1>s2)
-        cout<<"s1>s2";
-    else if (s1<s2)
-        cout<<"s1<s2";
-    else
-        cout<<"s1=s2";
-    
-    return 0; 
+
+    s2[2]='a';
+    s2[3]='t';
+
+    cout<<s1<<endl;
+    cout<<s2<<endl;
+
+    return 0;
 }
 
 STRING::STRING()
@@ -60,98 +53,100 @@ STRING::STRING()
 STRING::STRING(char *in_str)
 {
     length = strlen(in_str);
-    
+
     str = new char [length+1];
     if (!str) cout<<"Error Allocating Memory";
-    
+
     strcpy(str, in_str);
 }
 
 STRING::STRING(const STRING &ob)
 {
-    length = ob.get_length(); 
-    
+    length = ob.get_length();
+
     str = new char [length+1];
     if (!str) cout<<"Error Allocating Memory";
-    
-    strcpy(str, ob.get_str());    
+
+    strcpy(str, ob.get_str());
 }
 
 STRING::~STRING()
 {
-    delete [] str; 
+    delete [] str;
 }
 
 int STRING::get_length() const
 {
-    return length; 
+    return length;
 }
 
 char *STRING::get_str() const
 {
-    return str; 
+    return str;
 }
 
 void STRING::set_str(char *in_str)
 {
     if (str!=NULL)
-        delete str; 
-    
+        delete str;
+
     length = strlen(in_str);
-    
+
     str = new char [length];
     if (!str) cout<<"Error Allocating Memory";
-    
-    strcpy(str, in_str);    
+
+    strcpy(str, in_str);
 }
 
 void STRING::print()
 {
-    cout<<str; 
+    cout<<str;
 }
 
 STRING &STRING::operator=(STRING &right)
 {
-    if (this==&right) return *this; 
-    
-    length=right.length; 
-    
+    if (this==&right) return *this;
+
+    length=right.length;
+
     delete [] str;
     str = new char [length+1];
     if (!str) cout<<"Error Allocating Memory";
-    
+
     strcpy(str, right.str);
-    
-    return *this; 
-}
-char &STRING::operator[](int i){
-    if(i>=0 && i<=length)
-        return str[i];
-    else
-        cout<<"Index out of bounds";
+
+    return *this;
 }
 
-bool STRING::operator<(STRING &right){
-    
-    if (strcmp(str,right.str)<0)
-        return true;
-    else    
-        return false;
-        
-}
-bool STRING::operator>(STRING &right){
-    
-    if (strcmp(str,right.str)>0)
-        return true;
-    else    
-        return false;
-        
-}
-bool STRING::operator==(STRING &right){
-    
-    if(strcmp(str,right.str)==0)
-        return true;
+char &STRING::operator[](int i)
+{
+    if (i>=0 && i<=length)
+        return str[i];
     else
-        return false;
-        
+        cout<<"Index out of bounds!";
+}
+
+bool STRING::operator<(STRING &right)
+{
+    if (strcmp(str, right.str)<0)
+        return true;
+    return false;
+}
+
+bool STRING::operator>(STRING &right)
+{
+    if (strcmp(str, right.str)>0)
+        return true;
+    return false;
+}
+
+bool STRING::operator==(STRING &right)
+{
+    if (strcmp(str, right.str)==0)
+        return true;
+    return false;
+}
+ostream &operator<<(ostream &left, STRING &right) {
+    left<<right.str;
+    return left;
 }
